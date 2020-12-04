5febe8ac356da2e05daa88bc0c89e0a073270161
daaniell <59689738+daaniell@users.noreply.github.com>
2020-12-04 19:24:56 +0200

    console todolist

---
+++ ToDoList/ToDoList/AppContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList
+{
+    class AppContext : DbContext
+    {
+        public DbSet<TaskList> Tasks { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=tasklistdb;trusted_Connection=True;");
+        }
+    }
+}
