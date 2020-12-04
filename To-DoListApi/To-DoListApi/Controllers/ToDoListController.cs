080aff51a79704d4c02180a33d696a8cccfd3425
daaniell <59689738+daaniell@users.noreply.github.com>
2020-12-04 19:28:56 +0200

    Add files via upload

---
+++ To-DoListApi/To-DoListApi/Controllers/ToDoListController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using To_DoListApi.Models;
+
+namespace To_DoListApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ToDoListController : ControllerBase
+    {
+        ToDoListContext db;
+        public ToDoListController(ToDoListContext context)
+        {
+            db = context;
+        }
+    }
+}
---
+++ To-DoListApi/To-DoListApi/Models/ToDoListContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace To_DoListApi.Models
+{
+    public class ToDoListContext : DbContext
+    {
+        public DbSet<ToDoList> TDList { get; set; }
+        public ToDoListContext(DbContextOptions<ToDoListContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
